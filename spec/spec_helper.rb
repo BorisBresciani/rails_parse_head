d0900becfcf189eb9e02b1d5c15ca7c8baf8f59c
Boris Bresciani <boris2bresciani@gmail.com>
2019-12-19 23:19:58 +0100

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 
 require 'bundler/setup'
 require 'rails_parse_head'
+require 'coveralls'
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
